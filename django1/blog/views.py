3b5e633ce92174762a2189b2a7e6760f7ed29252
Oskar s7826 <kekw.studia@gmail.com>
2026-02-02 21:30:10 +0100

    Django2

--- django1/blog/views.py
+++ django1/blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Category
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.filter(published=True).order_by('-created_at')
+    return render(request, 'blog/post_list.html', {'posts': posts, 'title': 'Najnowsze posty'})
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, published=True)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def category_posts(request, id):
+    category = get_object_or_404(Category, id=id)
+    posts = Post.objects.filter(categories=category, published=True)
+    return render(request, 'blog/post_list.html', {'posts': posts, 'title': f'Kategoria: {category.name}'})
